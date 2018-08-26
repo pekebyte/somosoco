eb110e7baad4f887ae359ec9787ebe490fd0012b
Pedro Molina <pedrom@pekebyte.com>
2018-08-26 06:52:40 -0500

    Including again constants file

---
+++ app/src/main/java/com/pekebyte/somosoco/ui/helpers/Constants.java
@@ -0,0 +1,8 @@
+package com.pekebyte.somosoco.ui.helpers;
+
+public class Constants {
+    public static String BLOGGER_KEY = "BLOGGER_API_KEY";
+    public static String BLOGGER_ID = "BLOGGER_ID";
+    public static String BASE_URL = "https://www.googleapis.com/blogger/v3/blogs/"+BLOGGER_ID+"/";
+
+}
