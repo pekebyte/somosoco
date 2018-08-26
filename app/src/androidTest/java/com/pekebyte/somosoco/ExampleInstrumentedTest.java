13af271f09cf3e789ab9dc5c06308d56742bd36f
Pedro Molina <pedrom@pekebyte.com>
2018-08-25 21:41:21 -0500

    fastlane

--- app/src/androidTest/java/com/pekebyte/somosoco/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pekebyte/somosoco/ExampleInstrumentedTest.java
@@ -2,11 +2,23 @@ package com.pekebyte.somosoco;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.pekebyte.somosoco.ui.activities.MainActivity;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +28,32 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void setUp() {
+        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+    }
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("pekebyte.com.somosoco", appContext.getPackageName());
+        assertEquals("com.pekebyte.somosoco", appContext.getPackageName());
     }
+
+
+    @Test
+    public void testTakeScreenshot() {
+        Screengrab.screenshot("before_button_click");
+
+        // Your custom onView...
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+
+        Screengrab.screenshot("after_button_click");
+    }
+
 }
